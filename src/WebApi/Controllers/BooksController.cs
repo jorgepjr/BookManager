c49dc0033bb9a05f556ef6dc090250fd6c8aea05
Jorge Paiva <jorgepaiva1806@gmail.com>
2024-01-23 16:11:34 -0400

    Implementacao da action para cadastrar o livro

---
+++ src/WebApi/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Dtos;
+using Application.UseCases.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly IBookModule _bookModule;
+        public BooksController(IBookModule bookModule)
+        {
+            _bookModule = bookModule;
+        }
+
+        [HttpPost("/Create")]
+        public async Task<IActionResult> Post([FromBody] BookDto bookDto)
+        {
+            if (!ModelState.IsValid)
+            {
+               return BadRequest(ModelState);
+            }
+
+           await _bookModule.Create(bookDto);
+
+            if(_bookModule.Response.Type == ResponseType.Error)
+            {
+                return BadRequest(_bookModule.Response.Message);
+            }
+
+            return Ok(_bookModule.Response.Message);
+        }
+    }
+}
