f3300e2625b5bcb0a0e7f5d7945693622c85d139
Jorge Paiva <jorgepaiva1806@gmail.com>
2024-01-23 16:00:21 -0400

    Criacao da camada de teste e de dominio

---
+++ src/Domain/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain;
+public class Book
+{
+    public Book(string code, string title, string author, string year, string genre, string publisher)
+    {
+        Code = code;
+        Title = title;
+        Author = author;
+        Year = year;
+        Genre = genre;
+        Publisher = publisher;
+        Active = true;
+    }
+
+    public Guid Id { get; private set; }
+    public string Code { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public string Year { get; private set; }
+    public string Genre { get; private set; }
+    public string Publisher { get; private set; }
+    public bool Active { get; private set; }
+}
