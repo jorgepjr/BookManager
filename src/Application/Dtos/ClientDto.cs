1b0e6fb65f9cbb7012b99d4a537d5b6336319979
Jorge Paiva <jorgepaiva1806@gmail.com>
2024-01-24 10:00:49 -0400

    implementa caso de uso para cadastrar cliente

---
+++ src/Application/Dtos/ClientDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos
+{
+    public class ClientDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/Application/UseCases/ClientModule.cs
@@ -0,0 +1,38 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Domain;
+
+namespace Application.UseCases
+{
+    public class ClientModule
+    {
+        private readonly IClientPersistence _clientPersistence;
+
+        public ResponseDto Response { get; set; } = new();
+
+        public ClientModule(IClientPersistence clientPersistence)
+        {
+            _clientPersistence = clientPersistence;
+        }
+
+        public async Task Create(ClientDto clientDto)
+        {
+            var client = new Client(clientDto.Name,clientDto.Email);
+
+            try
+            {
+                var successfulRequest = await _clientPersistence.Create(client);
+
+                if (successfulRequest)
+                {
+                    Response = new ResponseDto { Type = ResponseType.Success, Message = "Client registered successfully" };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response = new ResponseDto { Type = ResponseType.Error, Message = ex.Message };
+            }
+        }
+    }
+}
