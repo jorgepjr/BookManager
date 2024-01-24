2e527eaef1b78bb002588bda88c0784343b8ec20
Jorge Paiva <jorgepaiva1806@gmail.com>
2024-01-24 15:18:55 -0400

    Add Required em BookDto e ClientDto

--- src/Application/Dtos/BookDto.cs
+++ src/Application/Dtos/BookDto.cs
@@ -1,12 +1,27 @@
-﻿namespace Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos
 {
     public class BookDto
     {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Author { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Year { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Publisher { get; set; }
     }
 }
--- src/Application/Dtos/ClientDto.cs
+++ src/Application/Dtos/ClientDto.cs
@@ -1,9 +1,15 @@
-﻿namespace Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos
 {
     public class ClientDto
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field '{0}' is required")]
         public string Email { get; set; }
     }
 }
