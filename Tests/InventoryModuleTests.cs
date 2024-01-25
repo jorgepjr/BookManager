337c74f46ffc494d86eb32c741b9db944e9174b9
Jorge Paiva <jorgepaiva1806@gmail.com>
2024-01-24 23:02:32 -0400

    testes dos modulos de Inventario e clientes

---
+++ Tests/InventoryModuleTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Application.UseCases;
+using Application.UseCases.Interfaces;
+using Application.UseCases.Modules;
+using Domain;
+using Moq;
+using WebApi.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class InventoryModuleTests
+    {
+        [Fact]
+        public async Task DeveCriarInventarioDoLivro()
+        {
+            var clientId = Guid.NewGuid();
+            var livroId = Guid.NewGuid();
+
+            var clientModule = new Mock<IClientModule>();
+            var bookModule = new Mock<IBookModule>();
+            var inventoryPersistence = new Mock<IInventoryPersistence>();
+
+            var inventoryModule = new InventoryModule(clientModule.Object, bookModule.Object, inventoryPersistence.Object);
+
+            await inventoryModule.Create(new InventoryDto { BookId = Guid.NewGuid(), Quantity = 4 });
+
+            inventoryPersistence.Verify(x => x.Create(It.IsAny<Inventory>()), Times.Once());
+        }
+    }
+}
